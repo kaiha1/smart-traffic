6957b8d30cc4f89bce55c7aadf1f0a65ee77da8d
safae <safaeachchab305@gmail.com>
2026-08-01 23:48:02 +0100

    Add report mode state

--- components/ReportButton.tsx
+++ components/ReportButton.tsx
@@ -1,23 +1,22 @@
 "use client";
 
-import { useState } from "react"; 
-export default function ReportButton() {
-    const [isReporting, setIsReporting] = useState(false);
+type ReportButtonProps = {
+    isReporting: boolean;
+    setIsReporting: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-    const handleReport = () => {
-        setIsReporting(true);
-        // Simulate report submission
-        setTimeout(() => {
-            setIsReporting(false);
-        }, 2000);
-    };
+export default function ReportButton( {
+    isReporting,
+    setIsReporting,
+}: ReportButtonProps) {
+
 
     return (
         <button
             className ="absolute bottom-6 right-6 bg-red-600 text-white px-5
             py-3 rounded-full shadow-lg transition  
             "
-            onClick={handleReport}
+            onClick={() => setIsReporting(true)}
         >
             {isReporting ? "Click anywhere on the map..." : "Report an Incident"}
         </button>
--- components/map.tsx
+++ components/map.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import {useState} from "react";
 import dynamic from "next/dynamic";
 import ReportButton from "./ReportButton";  
 
@@ -9,10 +10,14 @@ loading: () => <p>Loading map...</p>
 });
 
 export default function Map() {
+  const [isReporting, setIsReporting] = useState(false);
   return (
     <div className="w-full h-[600px]  rounded-xl overflow-hidden">
       <LeafletMap />
-      <ReportButton />
+      <ReportButton 
+        isReporting={isReporting}
+        setIsReporting={setIsReporting}
+      />
     </div>
   );
 }
